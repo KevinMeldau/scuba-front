ec7794a437e65ac1a925dcbf3e8aa5d6658257b1
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-07 18:50:01 -0400

    Did some stuff

--- src/App.js
+++ src/App.js
@@ -82,11 +82,54 @@ export default function App() {
 			<form onSubmit={addLog}>
 				<label>
 					<span>Country</span>
-					<input
+					<select
 						name="country"
 						value={state.newLog.log}
 						onChange={handleChange}
-					/>
+					>
+						<option value="Aruba">Aruba</option>
+						<option value="Bahamas">Bahamas</option>
+						<option value="Belize">Belize</option>
+						<option value="Benin">Benin</option>
+						<option value="Bermuda">Bermuda</option>
+						<option value="Cambodia">Cambodia</option>
+						<option value="Canary Islands">Canary Islands</option>
+						<option value="Cayman Islands">Cayman Islands</option>
+						<option value="Cocos Island">Cocos Island</option>
+						<option value="Guam">Guam</option>
+						<option value="Hawaii">Hawaii</option>
+						<option value="Iceland">Iceland</option>
+						<option value="Indonesia">Indonesia</option>
+						<option value="Italy">Italy</option>
+						<option value="Jamaica">Jamaica</option>
+						<option value="Japan">Japan</option>
+						<option value="Jordan">Jordan</option>
+						<option value="Kazakhstan">Kazakhstan</option>
+						<option value="Maldives">Maldives</option>
+						<option value="Mauritius">Mauritius</option>
+						<option value="Mayotte">Mayotte</option>
+						<option value="Mexico">Mexico</option>
+						<option value="Morocco">Morocco</option>
+						<option value="Mozambique">Mozambique</option>
+						<option value="Myanmar">Myanmar</option>
+						<option value="Nambia">Nambia</option>
+						<option value="Nauru">Nauru</option>
+						<option value="Nepal">Nepal</option>
+						<option value="Paraguay">Paraguay</option>
+						<option value="Seychelles">Seychelles</option>
+						<option value="Sierra Leone">Sierra Leone</option>
+						<option value="Singapore">Singapore</option>
+						<option value="Slovakia">Slovakia</option>
+						<option value="Thailand">Thailand</option>
+						<option value="Togo">Togo</option>
+						<option value="Tuvalu">Tuvalu</option>
+						<option value="Uganda">Uganda</option>
+						<option value="United Kingdom">United Kingdom</option>
+						<option value="Yemen">Yemen</option>
+						<option value="Zaire">Zaire</option>
+						<option value="Zambia">Zambia</option>
+						<option value="Zimbabwe">Zimbabwe</option>
+					</select>
 				</label>
 				<label>
 					<span>Dive Site</span>
