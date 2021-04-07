bf3a99d39802ca0e2f2260da6a768cd0170f6ce0
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-07 12:13:57 -0400

    Front-end working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [state, setState] = useState({
+		logs: [{ log: "JavaScript", level: "4" }],
+		newLog: {
+			log: "",
+			level: "3",
+		},
+	});
+
+	async function getAppData() {
+		try {
+			const BASE_URL = "http://localhost:3001/api/logs";
+			const logs = await fetch(BASE_URL).then(res => res.json());
+			setState(prevState => ({
+				...prevState,
+				logs,
+			}));
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		getAppData();
+	}, []);
+
+	async function addLog(e) {
+		e.preventDefault();
+
+		const BASE_URL = "http://localhost:3001/api/logs";
+
+		const log = await fetch(BASE_URL, {
+			method: "POST",
+			headers: {
+				"Content-type": "Application/json",
+			},
+			body: JSON.stringify(state.newLog),
+		}).then(res => res.json());
+
+		setState(prevState => ({
+			logs: [...prevState.logs, log],
+			newLog: {
+				log: "",
+				level: "3",
+			},
+		}));
+	}
+
+	function handleChange(e) {
+		setState(prevState => ({
+			...prevState,
+			newLog: {
+				...prevState.newLog,
+				[e.target.name]: e.target.value,
+			},
+		}));
+	}
+
+	return (
+		<section>
+			<h2>Dive Logs</h2>
+			<hr />
+			{state.logs.map(s => (
+				<article key={s.log}>
+					<div>{s.log}</div> <div>{s.level}</div>
+				</article>
+			))}
+			<hr />
+			<form onSubmit={addLog}>
+				<label>
+					<span>SKILL</span>
+					<input
+						name="log"
+						value={state.newLog.log}
+						onChange={handleChange}
+					/>
+				</label>
+				<label>
+					<span>LEVEL</span>
+					<select
+						name="level"
+						value={state.newLog.level}
+						onChange={handleChange}
+					>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+				</label>
+				<button>ADD SKILL</button>
+			</form>
+		</section>
+	);
 }
-
-export default App;
