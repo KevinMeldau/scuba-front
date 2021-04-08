73105631bfc5373e4cd8d72e56612fafce6a4eff
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-08 15:15:55 -0400

    Add and style nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,34 @@ export default function Header() {
 		<div className="header">
 			<div className="nav-container">
 				<img src={companyLogo} alt="Dive logo" />
-				<div className="nav"></div>
+				<div className="nav">
+					<div className="nav-items">
+						<ul>
+							<a href="#">
+								<li>About</li>
+							</a>
+							<a href="#">
+								<li>Explore</li>
+							</a>
+							<a href="#">
+								<li>Dive Log</li>
+							</a>
+							<a href="#">
+								<li>Contact</li>
+							</a>
+						</ul>
+					</div>
+					<div className="nav-login">
+						<ul>
+							<a href="#">
+								<li>Login</li>
+							</a>
+							<a href="#">
+								<li>Register</li>
+							</a>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
