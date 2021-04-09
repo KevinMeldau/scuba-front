032f464af3d0d1690907e4f2051b6c862b272b0a
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-09 09:58:16 -0400

    Flex wrap cards

--- src/components/DiveSites.js
+++ src/components/DiveSites.js
@@ -2,11 +2,32 @@ import "../styles.css";
 
 export default function DiveSites() {
 	return (
-		<div className="dive-sites">
-			<div className="dive-sites">
-				<h2>Dive Sites near you</h2>
-				<hr />
-				<div className="dive-sites-container">
+		<section className="dive-sites">
+			<h2 className="h1">Dive Sites near you</h2>
+			<hr className="hr hr-dive-sites" />
+			<div className="dive-sites-container">
+				<div className="site-card">
+					<strong className="dive-site-title">ABC Dive</strong>
+					<p className="dive-site-add">
+						76 Smith Street, Atlanta 30032
+					</p>
+					<p className="dive-site-tel">555.054.7787</p>
+				</div>
+				<div className="site-card">
+					<strong className="dive-site-title">ABC Dive</strong>
+					<p className="dive-site-add">
+						76 Smith Street, Atlanta 30032
+					</p>
+					<p className="dive-site-tel">555.054.7787</p>
+				</div>
+				<div className="site-card">
+					<strong className="dive-site-title">ABC Dive</strong>
+					<p className="dive-site-add">
+						76 Smith Street, Atlanta 30032
+					</p>
+					<p className="dive-site-tel">555.054.7787</p>
+				</div>
+				<div className="site-card">
 					<strong className="dive-site-title">ABC Dive</strong>
 					<p className="dive-site-add">
 						76 Smith Street, Atlanta 30032
@@ -14,6 +35,6 @@ export default function DiveSites() {
 					<p className="dive-site-tel">555.054.7787</p>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
