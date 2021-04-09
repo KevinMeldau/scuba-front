b7516f48bd12bada5d4ebc0dc1fdd2b5ccddcba0
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-09 09:17:06 -0400

    Added DiveSites component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import DiveSites from "./components/DiveSites";
 import LoggedDives from "./components/LoggedDives";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
 import "./styles.css";
 
 export default function App() {
 	return (
 		<div className="container">
 			<Header />
+			<DiveSites />
 			<LoggedDives />
 			<Footer />
 		</div>
---
+++ src/components/DiveSites.js
@@ -0,0 +1,19 @@
+import "../styles.css";
+
+export default function DiveSites() {
+	return (
+		<div className="dive-sites">
+			<div className="dive-sites">
+				<h2>Dive Sites near you</h2>
+				<hr />
+				<div className="dive-sites-container">
+					<strong className="dive-site-title">ABC Dive</strong>
+					<p className="dive-site-add">
+						76 Smith Street, Atlanta 30032
+					</p>
+					<p className="dive-site-tel">555.054.7787</p>
+				</div>
+			</div>
+		</div>
+	);
+}
