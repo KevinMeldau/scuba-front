c4a04e4ed5f46f536092e023d0c11832cb3cdc3b
Kevin Meldau <kevinmeldau@Kevins-MacBook-Pro.local>
2021-04-12 16:04:18 -0400

    Testing 3rd party API - Dive Sites

--- src/components/DiveSites.js
+++ src/components/DiveSites.js
@@ -1,42 +1,51 @@
+import { useState, useEffect } from "react";
 import "../styles.css";
 
 export default function DiveSites() {
-	return (
-		<section className="dive-sites">
-			<h2 className="h1">
-				<span>Dive</span> Sites near you
-			</h2>
-			<hr className="hr" />
-			<div className="dive-sites-container">
-				<div className="site-card">
-					<strong className="dive-site-title">ABC Dive</strong>
-					<p className="dive-site-add">
-						76 Smith Street, Atlanta 30032
-					</p>
-					<p className="dive-site-tel">555.054.7787</p>
-				</div>
-				<div className="site-card">
-					<strong className="dive-site-title">ABC Dive</strong>
-					<p className="dive-site-add">
-						76 Smith Street, Atlanta 30032
-					</p>
-					<p className="dive-site-tel">555.054.7787</p>
-				</div>
-				<div className="site-card">
-					<strong className="dive-site-title">ABC Dive</strong>
-					<p className="dive-site-add">
-						76 Smith Street, Atlanta 30032
-					</p>
-					<p className="dive-site-tel">555.054.7787</p>
-				</div>
-				<div className="site-card">
-					<strong className="dive-site-title">ABC Dive</strong>
-					<p className="dive-site-add">
-						76 Smith Street, Atlanta 30032
-					</p>
-					<p className="dive-site-tel">555.054.7787</p>
-				</div>
-			</div>
-		</section>
-	);
+  const [dives, setDives] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    const BASE_URL =
+      "https://cors-anywhere.herokuapp.com/http://api.divesites.com";
+    fetch(BASE_URL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("DATA: ", data);
+        setDives(data.sites);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("ERROR: ", error);
+      });
+  };
+
+  if (loading) return <div />;
+
+  return (
+    <section className="dive-sites">
+      <h2 className="h1">
+        <span>Dive</span> Sites near you
+      </h2>
+      <hr className="hr" />
+      <div className="dive-sites-container">
+        <div className="site-card">
+          {dives.map((s) => (
+            <div key={s.id}>
+              <div className="dive-site-title">{s.name}</div>
+              <p className="dive-site-add">
+                {s.distance} Miles from current location
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
